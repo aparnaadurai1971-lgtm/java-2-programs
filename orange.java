4c8a30ab4db68b1563fe85206188531d808ebdc3
Aparnaa D <aparnaadurai1971@gmail.com>
2026-06-01 16:37:27 +0530

    Add files via upload

---
+++ orange.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class orange{
+public static void main(String[]args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter your name:");
+String name = sc.nextLine();
+System.out.println("Welcome"+name);
+System.out.println("Enter your age:");
+int age = sc.nextint();
+print.out.println("age"+"years old")
+}
+}
